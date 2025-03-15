faaeae4fb674be7bb3eaab94040c8e185744b3b6
edsonpolucena <edsonpolucena@hotmail.com>
2025-03-15 18:25:19 -0300

    adicionado o programa bubble_sort

--- sorting_comparison/strategies/basico/bubble_sort.py
+++ sorting_comparison/strategies/basico/bubble_sort.py
@@ -1,20 +1,24 @@
-from sort_algorithms.sorting_strategy import SortingStrategy
+from utils.sorting_strategy import SortingStrategy
 
 class BubbleSort(SortingStrategy):
-    
-    def sort(self, data):
-        n = len(data)
-        comparisons, swaps = 0, 0
-        
+    def __init__(self):
+        self.comparisons = 0
+        self.swaps = 0
+
+    def sort(self, array):
+        n = len(array)
         for i in range(n):
             swapped = False
-            for j in range(0, n-i-1):
-                comparisons += 1
-                if data[j] > data[j+1]:
-                    data[j], data[j+1] = data[j+1], data[j]
-                    swaps += 1
+            for j in range(0, n - i - 1):
+                self.comparisons += 1
+                if array[j] > array[j + 1]:
+                    array[j], array[j + 1] = array[j + 1], array[j]
+                    self.swaps += 1
                     swapped = True
             if not swapped:
-                break
-        
-        return data, comparisons, swaps
+                break  # Otimização para sair caso o array já esteja ordenado
+        return array
+
+    def get_metrics(self):
+        return {"comparisons": self.comparisons, "swaps": self.swaps}
+
